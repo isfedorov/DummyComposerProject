bc1b47567cc8621b3c52f270f428245e9bf1ffd2
Ivan Fedorov <isfedorov@users.noreply.github.com>
2021-03-12 08:38:56 +0000

    Apply fixes from StyleCI

--- src/TestClass.php
+++ src/TestClass.php
@@ -3,19 +3,19 @@
  * Created by PhpStorm.
  * User: ifedorov
  * Date: 20/03/2018
- * Time: 20:14
+ * Time: 20:14.
  */
 
 namespace Composer\Testing;
 
 /**
- * Class TestClass
- * @package Composer\Testing
+ * Class TestClass.
+ *
  * @deprecated 5.6
  */
 class TestClass
 {
-    public function __construct(){
-
+    public function __construct()
+    {
     }
 }
