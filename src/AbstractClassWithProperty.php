3aad60e91e6353ee58ae32f016f380a49b68565c
Ivan.Fedorov <ivan.fedorov@jetbrains.com>
2024-12-17 12:14:26 +0100

    add class with properties

---
+++ src/AbstractClassWithProperty.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DummyProject;
+
+class AbstractClassWithProperty
+{
+    private int $privateProperty = 1;
+    protected int $protectedProperty = 2;
+    public int $publicProperty = 3;
+}
